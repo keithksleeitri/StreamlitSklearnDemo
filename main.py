0b6c4ab5e68c056b5a182456e8a4fb50d312a71c
Keith K.S. Lee <keith.kslee@gmail.com>
2024-02-13 15:51:46 +0800

    Basic scikit-learn model training

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
+from typing import Literal
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
+LABEL_NAME = 'Class variable'
 
 st.title('Diabetes')
 
 df = pd.read_csv('diabetes.csv')
 st.dataframe(df)
 
-fig = go.Figure(data=[go.Pie(labels=['negative', 'positive'], values=[(df['Class variable'] == 0).sum(), (df['Class variable'] == 1).sum()])])
-st.write([(df['Class variable'] == 0).sum(), (df['Class variable'] == 1).sum()])
+fig = go.Figure(data=[go.Pie(labels=['negative', 'positive'], values=[(df[LABEL_NAME] == 0).sum(), (df[LABEL_NAME] == 1).sum()])])
+st.write([(df[LABEL_NAME] == 0).sum(), (df[LABEL_NAME] == 1).sum()])
 st.plotly_chart(fig)
+
+def get_model(model_type: Literal['SVM', 'Logistic Regression', 'Decision Tree', 'Random Forest'], *arg, **kwargs) -> BaseEstimator:
+    if model_type == 'SVM':
+        return LinearSVC(*arg, **kwargs)
+    elif model_type == 'Decision Tree':
+        return DecisionTreeClassifier(*arg, **kwargs)
+    elif model_type == 'Logistic Regression':
+        return LogisticRegression(*arg, **kwargs)
+    elif model_type == 'Random Forest':
+        return RandomForestClassifier(*arg, **kwargs)
+
+model_type: Literal['SVM', 'Logistic Regression', 'Decision Tree', 'Random Forest'] = st.selectbox('Choose model', ['SVM', 'Logistic Regression', 'Decision Tree', 'Random Forest'])
+model = get_model(model_type)
+st.write(model)
+
+# Assuming df is your DataFrame and 'label' is the target column
+features = df.drop(LABEL_NAME, axis=1)
+labels = df[LABEL_NAME]
+
+# Split the data into training and test sets
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Train the model
+model.fit(features_train, labels_train)
+
+# Now your model is ready to make predictions
+predictions = model.predict(features_test)
+
+st.text('Predict')
+st.write(predictions)
+st.text('Label')
+st.write(labels_test)
+
+acc = accuracy_score(labels_test, predictions)
+st.metric('Accuracy', acc)
